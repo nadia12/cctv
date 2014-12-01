b3d5454891b7722ec557eb42f11a39b20b476fe4
alamtopan <alamtopani@yahoo.com>
2014-12-02 01:42:10 +0700

    revision

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-CctvProject::Application.configure do
+ProductionCctc::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
