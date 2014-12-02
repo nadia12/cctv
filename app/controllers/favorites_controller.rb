99d98c79f3f7b8741c63dfe72c474efe93535463
nadia12 <nadia.riantini@gmail.com>
2014-12-02 16:18:59 +0700

    revsiion

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < InheritedResources::Base
   def up
   	fav = current_user.favorites.new(channel_id: params[:favorite][:channel_id], status: 'Favorite')
   	fav.save
-  	redirect_to favorite_path, :notice => "You just set favorite CCTV" 
+  	redirect_to my_favorite_path, :notice => "You just set favorite CCTV" 
   end
 
   def down
   	current_user.favorites.where(channel_id: params[:favorite][:channel_id]).first.destroy
 
-  	redirect_to favorite_path, :notice => "You just removed favorite CCTV" 
+  	redirect_to my_favorite_path, :notice => "You just removed favorite CCTV" 
   end
 
 end
